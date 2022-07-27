cf2a0e366d555c96f8bae76c7329f65e9de2f62c
msfiqih <msfiqih@gmail.com>
2022-07-27 09:47:07 +0700

    Menambah authentikasi

---
+++ app/Http/Middleware/IsRekamMedis.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsRekamMedis
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (auth()->guest() || auth()->user()->username != 'admin' &&
+            auth()->user()->username != 'rm'
+            && auth()->user()->username != 'casemix'
+            && auth()->user()->username != 'admin_casemix'
+            && auth()->user()->username != 'direksi'
+
+        ) {
+            return redirect('/');
+        }
+
+        return $next($request);
+    }
+}
