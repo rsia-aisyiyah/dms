7dfc99e258c150ef6c2999d95241fa3364e9f371
msfiqih <msfiqih@gmail.com>
2022-11-19 11:12:04 +0700

    tindakan dokter

--- app/Http/Controllers/DokterController.php
+++ app/Http/Controllers/DokterController.php
@@ -7,6 +7,17 @@ use Illuminate\Http\Request;
 
 class DokterController extends Controller
 {
+    public function semuaDokter()
+    {
+        $dokter = Dokter::where('status', '1')
+            ->whereHas('pegawai', function ($q) {
+                $q->where('jbtn', '!=', '-')
+                    ->whereIn('jnj_jabatan', ['RS10', 'DIRU']);
+            })
+            ->get();
+
+        return response()->json($dokter);
+    }
     public function getDokterSpesialis()
     {
         $data = Dokter::whereIn('kd_sps', ['S0001', 'S0003'])
