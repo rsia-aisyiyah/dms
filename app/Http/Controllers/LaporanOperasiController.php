53c00164e63e89746a5fc947f5798d00a568d9d6
msfiqih <msfiqih@gmail.com>
2022-11-24 08:20:12 +0700

    update laporan operasi

---
+++ app/Http/Controllers/LaporanOperasiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Operasi;
+use Illuminate\Http\Request;
+
+class LaporanOperasiController extends Controller
+{
+    //
+
+}
